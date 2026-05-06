21c3a31d309fb0fac5c263054997d73d2807e035
renangth <renan.alima99@gmail.com>
2026-05-05 22:14:01 -0300

    adiciona funcionalidade ao menu e adiciona a opçao de criar arquivos em txt

--- src/main/java/com/analizador_de_logs/AnalisadorLog.java
+++ src/main/java/com/analizador_de_logs/AnalisadorLog.java
@@ -1,4 +1,39 @@
 package com.analizador_de_logs;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class AnalisadorLog {
+    private LeitorLog leitorLog;
+    private EscritorArquivo escritorArquivo;
+
+    public void RecursosGrandes(){
+        try{
+            FileWriter arquivoGrande = new FileWriter("recursosGrandes.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void NaoRespondidas(){
+        try {
+            FileWriter arquivoNaoRespondido = new FileWriter("naoRespondidosNovembro.txt");
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+
+    }
+
+    public void SistemasOperacionais(){
+        try{
+            FileWriter arquivoSO = new FileWriter("sistemasOperacionais.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void MediaPost(){
+
+    }
 }
--- src/main/java/com/analizador_de_logs/Menu.java
+++ src/main/java/com/analizador_de_logs/Menu.java
@@ -1,4 +1,41 @@
 package com.analizador_de_logs;
 
+import java.util.Scanner;
+
 public class Menu {
+    private Scanner scanner = new Scanner(System.in);
+    private AnalisadorLog analisador;
+    int opcao = scanner.nextInt();
+
+    public void iniciar(){
+        exibirMenu();
+        do{
+            switch (opcao){
+                case 1:
+                    analisador.RecursosGrandes();
+                    break;
+                case 2:
+                    analisador.NaoRespondidas();
+                    break;
+                case 3:
+                    analisador.SistemasOperacionais();
+                case 4:
+                    analisador.MediaPost();
+            }
+        } while (opcao == 0);
+    }
+
+    private void exibirMenu(){
+        System.out.println("""
+                ======== Menu ========
+                1 - Recursos grandes respondidos
+                2 - Não respondido
+                3 - % de requisitosMédia das requisições por SO
+                0 - Sair""");
+    }
+
+    private int lerOpcao(){
+
+        return 0;
+    }
 }
